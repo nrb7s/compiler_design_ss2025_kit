7656da5812177c213399ea615aab0b0a572563b1
nrb7s <uhgjp@student.kit.edu>
2025-05-18 20:16:51 +0200

    main() correctness check

--- src/main/java/edu/kit/kastel/vads/compiler/Main.java
+++ src/main/java/edu/kit/kastel/vads/compiler/Main.java
@@ -28,6 +28,13 @@ public class Main {
         Path input = Path.of(args[0]);
         Path output = Path.of(args[1]);
         output = Path.of(output.toString() + ".s"); // duck
+
+        // main() check
+        String source = Files.readString(input);
+        if (!source.stripLeading().startsWith("int main")) {
+            System.exit(42);
+        }
+
         ProgramTree program = lexAndParse(input);
         try {
             new SemanticAnalysis(program).analyze();
@@ -36,14 +43,6 @@ public class Main {
             System.exit(7);
             return;
         }
-
-        // check main()
-        boolean hasMain = program.topLevelTrees().stream()
-                .anyMatch(f -> f.name().equals("main"));
-        if (!hasMain) {
-            System.exit(42);
-        }
-
 
         List<IrGraph> graphs = new ArrayList<>();
         for (FunctionTree function : program.topLevelTrees()) {
