e76d18de718b81b25e42a99cb8a3c93490d96c51
nrb7s <uhgjp@student.kit.edu>
2025-06-11 00:48:35 +0200

    block now maintains a list of nodes

--- src/main/java/edu/kit/kastel/vads/compiler/ir/node/Block.java
+++ src/main/java/edu/kit/kastel/vads/compiler/ir/node/Block.java
@@ -1,11 +1,21 @@
 package edu.kit.kastel.vads.compiler.ir.node;
 
 import edu.kit.kastel.vads.compiler.ir.IrGraph;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Block extends Node {
+    private final List<Node> nodes = new ArrayList<>();
 
     public Block(IrGraph graph) {
         super(graph);
     }
 
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+
+    public List<Node> nodes() {
+        return nodes;
+    }
 }
