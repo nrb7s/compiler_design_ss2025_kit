fde45c16212704808633d50497cd05e6fae6a120
nrb7s <uhgjp@student.kit.edu>
2025-06-15 12:53:33 +0200

    visit update

--- src/main/java/edu/kit/kastel/vads/compiler/semantic/Namespace.java
+++ src/main/java/edu/kit/kastel/vads/compiler/semantic/Namespace.java
@@ -6,6 +6,7 @@ import org.jspecify.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.BinaryOperator;
 
 public class Namespace<T> {
@@ -22,8 +23,28 @@ public class Namespace<T> {
         return forked;
     }
 
+    public void mergeExisting(Namespace<T> other, BinaryOperator<T> merger) {
+        for (Map.Entry<Name, T> e : other.content.entrySet()) {
+            if (this.content.containsKey(e.getKey())) {
+                this.content.merge(e.getKey(), e.getValue(), merger);
+            }
+        }
+    }
+
+    public Set<Name> names() {
+        return this.content.keySet();
+    }
+
     public void put(NameTree name, T value, BinaryOperator<T> merger) {
-        this.content.merge(name.name(), value, merger);
+        put(name.name(), value, merger);
+    }
+
+    public void put(Name name, T value, BinaryOperator<T> merger) {
+        this.content.merge(name, value, merger);
+    }
+
+    public @Nullable T get(Name name) {
+        return this.content.get(name);
     }
 
     public @Nullable T get(NameTree name) {
