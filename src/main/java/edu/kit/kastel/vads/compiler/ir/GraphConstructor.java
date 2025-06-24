bfcd1a28bc7c9d673616f9d0efd93afe299ea402
nrb7s <uhgjp@student.kit.edu>
2025-06-24 21:25:27 +0200

    test

--- src/main/java/edu/kit/kastel/vads/compiler/ir/GraphConstructor.java
+++ src/main/java/edu/kit/kastel/vads/compiler/ir/GraphConstructor.java
@@ -187,8 +187,8 @@ public class GraphConstructor {
 
     void writeVariable(Name variable, Block block, Node value) {
         // System.out.println("WRITE " + variable.asString() + " in " + block.getId() + " -> " + value);
+        if (this.graph.origin(value) == null) {this.graph.setOrigin(value, variable);}
         this.currentDef.computeIfAbsent(variable, _ -> new HashMap<>()).put(block, value);
-        // if (this.graph.origin(value) == null) {this.graph.setOrigin(value, variable);}
     }
 
     Node readVariable(Name variable, Block block) {
